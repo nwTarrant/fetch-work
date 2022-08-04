625ce62991315c7076e0aa3bf967176343a087a7
nathan <98458521+nwTarrant@users.noreply.github.com>
2022-08-03 20:13:11 -0500

    Experimental change to .mockend.json

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("hellooooo");
+fetch('https://mockend.com/nwTarrant/fetch-work/posts/1')
+  .then(response => response.json())
+  .then(post => console.log(post))
+  .catch(() => console.log("Request failed"));
