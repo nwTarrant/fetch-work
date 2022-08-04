cc54a4af8ae25837af756dba8a2e086eaf16551f
nathan <98458521+nwTarrant@users.noreply.github.com>
2022-08-03 20:35:03 -0500

    Remove recipe because it didn't work

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-fetch('https://mockend.com/nwTarrant/fetch-work/foods/2')
+fetch('https://mockend.com/nwTarrant/fetch-work/foods')
   .then(response => response.json())
   .then(post => console.log(post))
   .catch(() => console.log("Request failed"));
